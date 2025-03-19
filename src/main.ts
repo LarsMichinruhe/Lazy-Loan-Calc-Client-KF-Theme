0a263521e696f9f4920abf1b76de67eaf775d4cc
LarsMichinruhe <thomas.gerhards@bms-cs.de>
2025-03-19 10:46:44 +0100

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { LoanCalculatorComponent } from './app/app.component';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
+
 
 bootstrapApplication(LoanCalculatorComponent, {
-	providers: [provideHttpClient()]
+	providers: [provideHttpClient(), provideEnvironmentNgxMask()]
 })
 	.catch((err) => console.error(err));
 	
